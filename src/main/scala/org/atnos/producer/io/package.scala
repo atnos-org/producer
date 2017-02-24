d2e0d110d6fdc1ec00fca3f00a465815aa920627
Eric Torreborre <etorreborre@yahoo.com>
2017-02-24 11:58:35 +0100

    added io functions to read bytes directly

--- src/main/scala/org/atnos/producer/io/package.scala
+++ src/main/scala/org/atnos/producer/io/package.scala
@@ -8,22 +8,28 @@ import org.atnos.eff.all._
 
 package object io {
 
+  def readBytes[R :_Safe](path: String, size: Int = 4096, chunkSize: Int = 100): Producer[R, Array[Byte]] =
+    bracket(openDataInputStream[R](path, size))(readerBytes(size, chunkSize))(closeDataInputStream)
+
   def readLines[R :_Safe](path: String, encoding: String = "UTF-8", size: Int = 4096, chunkSize: Int = 100): Producer[R, String] =
-    bracket(openFile[R](path, encoding, size))(readerLines(chunkSize))(closeFile)
+    bracket(openBufferedReader[R](path, encoding, size))(readerLines(chunkSize))(closeBufferedReader)
 
   def readLinesFromStream[R :_Safe](path: InputStream, encoding: String = "UTF-8", size: Int = 4096, chunkSize: Int = 100): Producer[R, String] =
-    bracket(openStream[R](path, encoding, size))(readerLines(chunkSize))(closeFile)
+    bracket(openBufferedReaderFromInputStream[R](path, encoding, size))(readerLines(chunkSize))(closeBufferedReader)
 
   def writeLines[R :_Safe](path: String, encoding: String = "UTF-8")(producer: Producer[R, String]): Eff[R, Unit] =
     producer.fold[Unit, BufferedWriter](protect[R, BufferedWriter](new BufferedWriter(new OutputStreamWriter(new FileOutputStream(path), encoding))),
-      (writer, line) => { writer.write(line+"\n"); writer },
-      writer => protect(writer.close))
+      (writer: BufferedWriter, line: String) => protect { writer.write(line+"\n"); writer },
+      (writer: BufferedWriter) => protect(writer.close))
 
-  def openFile[R :_Safe](path: String, encoding: String, size: Int = 4096): Eff[R, BufferedReader] =
+  def openBufferedReader[R :_Safe](path: String, encoding: String, size: Int = 4096): Eff[R, BufferedReader] =
     protect[R, BufferedReader](new BufferedReader(new InputStreamReader(new FileInputStream(path), encoding), size))
 
-  def openStream[R :_Safe](stream: InputStream, encoding: String, size: Int = 4096): Eff[R, BufferedReader] =
+  def openBufferedReaderFromInputStream[R :_Safe](stream: InputStream, encoding: String, size: Int = 4096): Eff[R, BufferedReader] =
     protect[R, BufferedReader](new BufferedReader(new InputStreamReader(stream, encoding), size))
+
+  def openDataInputStream[R :_Safe](path: String, size: Int = 4096): Eff[R, DataInputStream] =
+    protect[R, DataInputStream](new DataInputStream(new FileInputStream(path)))
 
   def readerLines[R :_Safe]: BufferedReader => Producer[R, String] =
     readerLines[R](100)
@@ -47,7 +53,30 @@ package object io {
         else Some((r, NonEmptyList.fromListUnsafe(lines.toList)))
       }
 
-  def closeFile[R :_Safe] = (reader: BufferedReader) =>
+  def readerBytes[R :_Safe](size: Int, chunkSize: Int): DataInputStream => Producer[R, Array[Byte]] =
+    (stream: DataInputStream) => {
+      val array = Array.ofDim[Byte](size)
+      Producer.unfoldList[R, DataInputStream, Array[Byte]](stream) { s =>
+        val arrays = new collection.mutable.ListBuffer[Array[Byte]]
+        var continue = true
+
+        while (continue) {
+          if (arrays.size < chunkSize) {
+            val result = s.read(array)
+            if (result > 0) arrays.append(array.clone)
+            else continue = false
+          } else continue = false
+        }
+
+        if (arrays.isEmpty) None
+        else Some((s, NonEmptyList.fromListUnsafe(arrays.toList)))
+      }
+    }
+
+  def closeBufferedReader[R :_Safe] = (reader: BufferedReader) =>
     protect[R, Unit](reader.close)
+
+  def closeDataInputStream[R :_Safe] = (stream: DataInputStream) =>
+    protect[R, Unit](stream.close)
 
 }
