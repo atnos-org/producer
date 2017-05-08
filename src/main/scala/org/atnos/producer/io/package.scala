ffe396b41de89ca9f953b5e4060a130e1dc72ce6
Eric Torreborre <etorreborre@yahoo.com>
2017-05-08 12:41:52 +0200

    added a method to read byte asynchronously

--- src/main/scala/org/atnos/producer/io/package.scala
+++ src/main/scala/org/atnos/producer/io/package.scala
@@ -3,13 +3,19 @@ package org.atnos.producer
 import java.io._
 
 import cats.data.NonEmptyList
-import org.atnos.eff.Eff
+import org.atnos.eff.{Eff, ExecutorServices}
 import org.atnos.eff.all._
+import org.atnos.eff.future._
+
+import scala.concurrent.ExecutionContext
 
 package object io {
 
   def readBytes[R :_Safe](path: String, size: Int = 4096, chunkSize: Int = 100): Producer[Eff[R, ?], Array[Byte]] =
     bracket(openDataInputStream[R](path, size))(readerBytes(size, chunkSize))(closeDataInputStream)
+
+  def readBytesAsync[R :_Safe :_future](path: String, size: Int = 4096, chunkSize: Int = 100)(es: ExecutorServices): Producer[Eff[R, ?], Array[Byte]] =
+    bracket(openDataInputStream[R](path, size))(readerBytesAsync(size, chunkSize, es))(closeDataInputStream)
 
   def readLines[R :_Safe](path: String, encoding: String = "UTF-8", size: Int = 4096, chunkSize: Int = 100): Producer[Eff[R, ?], String] =
     bracket(openBufferedReader[R](path, encoding, size))(readerLines(chunkSize))(closeBufferedReader)
@@ -57,19 +63,15 @@ package object io {
     (stream: DataInputStream) => {
       val array = Array.ofDim[Byte](size)
       Producer.unfoldList[Eff[R, ?], DataInputStream, Array[Byte]](stream) { s =>
-        val arrays = new collection.mutable.ListBuffer[Array[Byte]]
-        var continue = true
+        readBytesArray(s, array, chunkSize)
+      }
+    }
 
-        while (continue) {
-          if (arrays.size < chunkSize) {
-            val result = s.read(array)
-            if (result > 0) arrays.append(array.clone)
-            else continue = false
-          } else continue = false
-        }
-
-        if (arrays.isEmpty) None
-        else Some((s, NonEmptyList.fromListUnsafe(arrays.toList)))
+  def readerBytesAsync[R :_Safe :_future](size: Int, chunkSize: Int, es: ExecutorServices): DataInputStream => Producer[Eff[R, ?], Array[Byte]] =
+    (stream: DataInputStream) => {
+      val array = Array.ofDim[Byte](size)
+      Producer.unfoldListM[Eff[R, ?], DataInputStream, Array[Byte]](stream) { s =>
+        futureFork(readBytesArray(s, array, chunkSize), es.executionContext)
       }
     }
 
@@ -78,5 +80,21 @@ package object io {
 
   def closeDataInputStream[R :_Safe] = (stream: DataInputStream) =>
     protect[R, Unit](stream.close)
+
+  private def readBytesArray(s: DataInputStream, array: Array[Byte], chunkSize: Int): Option[(DataInputStream, NonEmptyList[Array[Byte]])] = {
+    val arrays = new collection.mutable.ListBuffer[Array[Byte]]
+    var continue = true
+
+    while (continue) {
+      if (arrays.size < chunkSize) {
+        val result = s.read(array)
+        if (result > 0) arrays.append(array.clone)
+        else continue = false
+      } else continue = false
+    }
+
+    if (arrays.isEmpty) None
+    else Some((s, NonEmptyList.fromListUnsafe(arrays.toList)))
+  }
 
 }
